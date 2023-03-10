a53a436491f0714e40eb2f8f27a24ba2e2f04ea3
guschins <guschins@gmail.com>
2023-03-10 17:46:49 +0300

    feat: add routing

--- youtube-client-app/src/app/app-routing.module.ts
+++ youtube-client-app/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CardComponent } from './components/card/card.component';
+import { HeaderComponent } from './components/header/header.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'list', component: CardComponent},
+  {path: 'description', component: HeaderComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
