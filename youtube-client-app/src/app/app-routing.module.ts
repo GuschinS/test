c289521c788b4c1cf5a92d988f39318f22662957
guschins <guschins@gmail.com>
2023-03-16 22:36:18 +0300

    fix: change routing

--- youtube-client-app/src/app/app-routing.module.ts
+++ youtube-client-app/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
 import { CardComponent } from './components/card/card.component';
 import { CardDetailComponent } from './components/card-detail/card-detail.component';
 
 const routes: Routes = [
-  { path: 'list', component: CardComponent },
+  {path: 'main', component: HeaderComponent},
+  { path: 'search', component: CardComponent },
   { path: 'description/:id', component: CardDetailComponent },
-  { path: '', redirectTo: '/list', pathMatch: 'full' },
+  { path: '', redirectTo: '/main', pathMatch: 'full' },
 ];
 
 @NgModule({
